8232933951b2960fe61a75200c77f85fdafdf79b
isaac <oseasisaac91@gmail.com>
2019-03-28 15:20:44 -0400

    figaro included with some changes to the email template

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
 config.action_mailer.smtp_settings = {
  :address              => "smtp.gmail.com",
  :port                 => 587,
- :user_name            => '',
- :password             => '',
+ :user_name            => 'katysbdaywebsite@gmail.com',
+ :password             => ENV['GMAIL_PASSWORD'],
  :authentication       => "plain",
 :enable_starttls_auto => true
 }
