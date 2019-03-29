b89a11a6c6bb763138b5fb0c024b8b3f6bf8a141
isaac <oseasisaac91@gmail.com>
2019-03-29 09:18:48 -0400

    implements cloudinary to send an email with the canvas photo inside

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,9 +1,14 @@
+require "base64"
+require 'cloudinary'
+
 class PicturesController < ApplicationController
+
+
 
   def mail
     @picture = Picture.create(picture_params)
+    @picture.url = uploadToCloudinary(@picture.url)
     PictureMailer.send_pic(@picture).deliver_now
-
   end
 
   private
@@ -11,5 +16,9 @@ class PicturesController < ApplicationController
   def picture_params
     params.require(:picture).permit(:email, :url)
   end
+
+  def uploadToCloudinary(data)
+   Cloudinary::Uploader.upload(data)["url"]
+ end
 
 end
--- app/mailers/picture_mailer.rb
+++ app/mailers/picture_mailer.rb
@@ -3,7 +3,7 @@ class PictureMailer < ApplicationMailer
 
   def send_pic(picture)
 
-  @picture = picture
+    @picture = picture
     mail(to: @picture.email, subject: 'Test')
   end
 end
